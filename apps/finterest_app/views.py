bd3ad848f9cd1f0f70fd61073100683500913650
mattselden12 <matthewtedselden@gmail.com>
2018-07-24 11:19:21 -0700

    made models, migrated

--- apps/finterest_app/views.py
+++ apps/finterest_app/views.py
@@ -4,4 +4,13 @@ from .models import *
 import bcrypt
 
 def index(request):
-    pass
+    return render(request, 'finterest_app/index.html')
+
+def login(request):
+    return render(request, 'finterest_app/login.html')
+
+def dashboard(request):
+    return render(request, 'finterest_app/dashboard.html')
+
+def addnewfave(request):
+    return render(request, 'finterest_app/addnewfave.html')
